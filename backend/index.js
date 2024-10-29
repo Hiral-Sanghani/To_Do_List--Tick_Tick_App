015242b1cd2b314a95434cfd3fa35a7251f35357
Hiral Sanghani <46317729+Hiral-Sanghani@users.noreply.github.com>
2024-10-29 04:49:36 +0000

    minor code changes

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,27 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const taskRoutes = require("./routes/taskRoutes");
 const sequelize = require("./config/database");
 require("dotenv").config();
 
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: "https://q9jjt3-3000.csb.app",
+  })
+);
+
 app.use("/api", taskRoutes);
 
-sequelize.sync({ force: true }).then(() => {
-  app.listen(9000, () => {
-    console.log("Backend server running on http://localhost:9000");
+sequelize
+  .sync({ force: true })
+  .then(() => {
+    app.listen(9000, () => {
+      console.log("Backend server running on http://localhost:9000");
+    });
+  })
+  .catch((error) => {
+    console.error("Error syncing database:", error);
   });
-});
